9d721bbd38e0af5b653264c92c7bf7a73bec3048
Radoslav Stefanov <radoslav.g.stefanov@gmail.com>
2022-02-17 17:41:31 +0200

    Created the Data models and generated the Database

---
+++ SharedTrip/Data/Model/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedTrip.Data.Model
+{
+    public class User
+    {
+        [Key]
+        [StringLength(36)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string Password { get; set; }
+
+        public ICollection<UserTrip> UserTrips { get; set; }
+
+        public User()
+        {
+            UserTrips = new List<UserTrip>();
+        }
+    }
+}
